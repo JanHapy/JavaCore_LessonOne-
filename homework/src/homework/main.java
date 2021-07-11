07a2d2b908718ad4c3f63fbad6e47b4354a4641a
LogosItAcademy <Hacker@DESKTOP-4V2MC6M>
2021-07-11 15:28:09 +0300

    add first project to github

---
+++ homework/src/homework/main.java
@@ -0,0 +1,56 @@
+package homework;
+
+
+public class main {
+	public static void main(String[] args) {
+	
+		 // ----- first task ---	
+//	byte b = -18;
+//	short sh = 32767;
+//	int i = 21474647;
+//	long l = 92233724775807l;
+//	char c = 'c';
+//	float f = 22434.213f;
+//	double d = 321.42112d;
+//	boolean bool = true;
+      
+         
+         // ----- second task ---		
+//    System.out.println(Integer.MAX_VALUE);
+//    System.out.println(Integer.MIN_VALUE);
+//    
+//    System.out.println(Byte.MAX_VALUE);
+//    System.out.println(Byte.MIN_VALUE);
+//    
+//    System.out.println(Long.MAX_VALUE);   
+//    System.out.println(Long.MIN_VALUE); 
+//    
+//    System.out.println(Short.MAX_VALUE);   
+//    System.out.println(Short.MIN_VALUE); 
+//    
+//    System.out.println(Float.MAX_VALUE);   
+//    System.out.println(Float.MIN_VALUE);
+//    
+//    System.out.println(Double.MAX_VALUE);   
+//    System.out.println(Double.MIN_VALUE);
+
+    
+    
+         // ----- third task ----		
+//	int numbers[] = { 92, -18, 2, 120, 67, 4, -1, 9, 88, 1 };
+//	int min, max;
+//	min = max = numbers[0];
+//
+//	for (int i = 1; i < 10; i++) {
+//	     if (numbers[i] < min)
+//	         min = numbers[i];
+//	     if(numbers[i] > max){
+//	         max = numbers[i];
+//	        }
+//	    }
+//	System.out.println("min is: " + min + "; max is: " + max);
+		
+	//first commit to github
+	
+	}
+}
